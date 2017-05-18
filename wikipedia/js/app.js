ba10c05679f0b31b6b0b3b436d6d6b2365f7ec7c
LucaScanu <lucagscanu@gmail.com>
2017-05-18 22:37:38 +0100

    display ajax data onto html

--- wikipedia/js/app.js
+++ wikipedia/js/app.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
-  $.ajax({
-    url:
-    "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=rome",
-    dataType: "jsonp",
-    success: function(response) {
-       console.log(response);
-     },
-   error: function () {
-    alert("Error retrieving search results, please refresh the page");
-   }
-  });
-  function showResponse(response) {
-    
+
+  $('#button').on('click', ajax);
+
+  function ajax() {
+    var keyword = $("#keyword").val();
+    // console.log(keyword);
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json&callback=?",
+      dataType: "jsonp",
+      success: function(data) {
+        console.log(data);
+        $('.content').html('');
+        for(var i=0; i<data[1].length; i++){
+        $('.content').prepend("<li><a href=" +data[3][i]+ ">" + data[1][i] + "</a></li>");
+        }
+      },
+      error: function() {
+        alert("Error retrieving search results, please refresh the page");
+      }
+    });
   }
 });
+
+
+// "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + keyword + ""
