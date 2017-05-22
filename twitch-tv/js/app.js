19b4f4fbac7b9b07c9204512efb25f68e8a55c74
LucaScanu <lucagscanu@gmail.com>
2017-05-22 22:02:36 +0100

    displayed status data, logo and channel name onto html page

--- twitch-tv/js/app.js
+++ twitch-tv/js/app.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
 
   var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
-  for (var i=0; i<channels.length; i++) {
-  $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/' +channels[i], function(data) {
-    console.log(data);
+  channels.forEach(function(channel) {
+  $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/' +channel, function(data) {
+    console.log(data, data.display_name, data.mature, data.logo);
+    $('#name').append('<li>' +channel+ '</li>');
+    $('#logo').append('<li><img src=' +data.logo+ '></li>');
+    if(data.mature === false || data.mature === null) {
+      $('#status').append('<li>Offline</li>')
+    } else {
+      $('#status').append('<li>Live</li>')
+    }
   });
-}
+});
 
 });
