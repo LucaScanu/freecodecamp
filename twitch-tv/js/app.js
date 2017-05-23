41ff538710c67745c54a24db0bbb2e8387e81f57
LucaScanu <lucagscanu@gmail.com>
2017-05-23 10:25:26 +0100

    added channels link to their twitch account

--- twitch-tv/js/app.js
+++ twitch-tv/js/app.js
@@ -1,17 +1,17 @@
 $(document).ready(function ajax() {
 
-  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"]
+  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"]
 
   channels.forEach(function(channel) {
   $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/' +channel, function(data) {
-    console.log(data, data.display_name, data.mature, data.logo);
+    console.log(data, data.display_name, data.url, data.logo);
     $('#name').append('<li>' +channel+ '</li>');
-    $('#logo').append('<li><img src=' +data.logo+ '></li>');
+    $('#logo').append('<li id="left"><img src=' +data.logo+ '></li>');
     if(data.mature === false || data.mature === null) {
-      $('#status').append('<li id="status">Offline</li>');
+      $('#status').append('<li id="right"><a href=' +data.url+ ' target="blank" class="offline">Offline</a></li>');
       $('#content').append('<li>No content available</li>');
     } else {
-      $('#status').append('<li>Live</li>');
+      $('#status').append('<li id="right"><a href=' +data.url+ ' target="blank" class="live">Live</li>');
       $('#content').append('<li>' +data.status+ '</li>');
     }
   });
