f01cf9b6e944b3b1bcdfd14d79d04c51e392c7ea
LucaScanu <lucagscanu@gmail.com>
2017-05-23 09:39:16 +0100

    added content data

--- twitch-tv/js/app.js
+++ twitch-tv/js/app.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function ajax() {
 
-  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
   channels.forEach(function(channel) {
   $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/' +channel, function(data) {
@@ -8,9 +8,11 @@ $(document).ready(function() {
     $('#name').append('<li>' +channel+ '</li>');
     $('#logo').append('<li><img src=' +data.logo+ '></li>');
     if(data.mature === false || data.mature === null) {
-      $('#status').append('<li>Offline</li>')
+      $('#status').append('<li id="status">Offline</li>');
+      $('#content').append('<li>No content available</li>');
     } else {
-      $('#status').append('<li>Live</li>')
+      $('#status').append('<li>Live</li>');
+      $('#content').append('<li>' +data.status+ '</li>');
     }
   });
 });
